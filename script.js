b27dff410229b2a49ba6fa603f3de4c0322e6943
Masoudfatemeh <masoudkhani.gm@gmail.com>
2026-08-02 14:43:52 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
 
+    const enterBtn = document.getElementById("enterBtn");
+
+    enterBtn.addEventListener("click", function(){
+
+        document.querySelector(".hero").style.opacity = "0";
+
+        setTimeout(function(){
+
+            window.location.href = "invite.html";
+
+        },800);
+
+    });
+
+});
